76794e304e33e2be4d32bb18a7e87ba2ac6d54fb
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-06-26 13:05:19 +0300

    add index.js, results.json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    return array.slice(0, 2);
+    
+}
+function returnLastTwoDrivers(array){
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(tin){
+    return function(date){
+        return tin * date;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, functions){
+    return functions(arrayOfDrivers); 
+}
